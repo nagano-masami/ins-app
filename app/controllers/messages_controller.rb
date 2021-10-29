31b25a9bc0c4a300fbc5eca6bc2e486c1620a1b6
S8799 <nagano.masami@jgc.com>
2021-10-29 18:01:35 +0900

    CRUD messages

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,63 @@
+class MessagesController < ApplicationController
+  def index
+    #Messageモデルのレコードの一覧表示
+    #@~~のインスタンス変数でControllerからViewへ変数を渡す
+    @messages = Message.all
+  end
+
+  def show
+    @message = Message.find(params[:id])
+  end
+
+  def new
+    #POSTメソッドを送信する新規作成用の入力フォーム置き場
+    @message = Message.new(content:'sample')
+  end
+
+  def create
+    #newのページから送信されるフォームを処理
+    @message = Message.new(message_params)
+
+    if @message.save
+      flash[:success] = 'Messageが正常に投稿されました'
+      redirect_to @message
+    else
+      flash.now[:danger] = 'Messageが投稿されませんでした'
+      render :new
+    end
+  end
+
+  def edit
+    #既存メッセージレコードを編集
+    @messege = Message.find(params[:id])
+  end
+
+  def update
+    @message = Message.find(params[:id])
+
+    if @message.update(message_params)
+      flash[:success] = 'Messageは正常に更新されました'
+      redirect_to @message
+    else
+      flash.now[:danger] = 'Messageは更新されませんでした'
+      render :edit
+    end
+  end
+
+  def destroy
+    @message = Message.find(params[:id])
+    @message.destroy
+
+    flash[:success] = 'Messegaは正常に削除されました'
+    redirect_to messages_url
+  end
+end
+
+private
+
+# Strong Parameter
+def message_params
+  #必要なパラメータを把握して、送信されてきたデータをフィルタリングする
+  #contentカラムだけが欲しいデータ
+  params.require(:message).permit(:content)
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,23 @@
 Rails.application.routes.draw do
-  get 'home/index'
-  root to: 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'messages#index'
+
+  resources :messages
+  #resources :messages,except:[:index]
+
+  #CRUD
+  #get 'messages/:id', to: 'messages#show'
+  #post 'messages', to: 'messages#create'
+  #put 'messages/:id', to: 'messages#update'
+  #delete 'messages/:id', to: 'messages#destroy'
+  #index:showの補助ページ
+  #get 'messages', to: 'messages#index'
+  #new:新規作成用のフォームページ
+  #get 'messages/new', to: 'messages#new'
+  #edit:更新用のフォームページ
+  #get 'messages/:id/edit', to: 'messages#edit'
+
+  #get 'home/index', to: 'home#index'
+  #root to: 'home#index'
+
 end
