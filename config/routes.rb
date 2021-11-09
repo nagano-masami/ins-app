0a74f014296f00b09fec92f9483f4b9358a0286e
S8799 <nagano.masami@jgc.com>
2021-11-09 10:16:43 +0900

    top page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get 'toppages/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'messages#index'
+  #root to: 'messages#index'
+  root to: 'toppages#index'
 
   resources :messages
   #resources :messages,except:[:index]
