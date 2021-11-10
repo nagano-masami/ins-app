b96730cfecd2a518878355aa895411408e4a790d
S8799 <nagano.masami@jgc.com>
2021-11-10 18:27:30 +0900

    follow unfollow

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_10_013830) do
+ActiveRecord::Schema.define(version: 2021_11_10_055927) do
 
   create_table "messages", force: :cascade do |t|
     t.string "content"
@@ -25,6 +25,16 @@ ActiveRecord::Schema.define(version: 2021_11_10_013830) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_microposts_on_user_id"
+  end
+
+  create_table "relationships", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "follow_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["follow_id"], name: "index_relationships_on_follow_id"
+    t.index ["user_id", "follow_id"], name: "index_relationships_on_user_id_and_follow_id", unique: true
+    t.index ["user_id"], name: "index_relationships_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
